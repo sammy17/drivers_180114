d9ba2501e0d0859571087fb955a6190386733480
sammy17 <chathura.abeyrathne.lk@gmail.com>
2018-01-14 21:34:01 +0530

    fir

---
+++ detection/BGSDetector.h
@@ -0,0 +1,75 @@
+//
+// Created by dilin on 10/23/17.
+//
+
+#ifndef TRACK_BGSDETECTOR_H
+#define TRACK_BGSDETECTOR_H
+
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include "Detector.h"
+#include "Histogram.h"
+
+//#define BGS_DEBUG_MODE
+
+class Blob
+{
+public:
+    Blob(std::vector<cv::Point> _contour);
+    std::vector<cv::Point> currentContour;
+
+    cv::Rect currentBoundingRect;
+
+    std::vector<cv::Point> centerPositions;
+
+    double dblCurrentDiagonalSize;
+    double dblCurrentAspectRatio;
+
+};
+
+enum
+{
+    BGS_MOVING_AVERAGE=1,
+    BGS_GMM=2
+};
+
+struct DetectionRecord
+{
+    float data[8];
+};
+
+class BGSDetector : public Detector
+{
+public:
+    explicit BGSDetector(double TH=30,
+                         int method=BGS_MOVING_AVERAGE,
+                         bool doGammaCorrection=false,
+                         string coeffFilePath="",
+                         bool trainingMode=false);
+    std::vector<cv::Rect> detect(cv::Mat &img);
+    vector<DetectionRecord> data;
+    void trainDetector();
+
+private:
+    void backgroundSubstraction(cv::Mat &frame0, cv::Mat &frame1, cv::Mat &frame2
+            , cv::Mat &bgModel, cv::Mat &mask, double TH=15);
+    cv::Mat frames[3];
+    cv::Mat bgModel;
+    cv::Mat mask;
+    uint8_t frameCount;
+    double TH;
+    // cv::Ptr<cv::BackgroundSubtractor> pMOG2; //MOG2 Background subtractor
+    void GammaCorrection(cv::Mat& src, cv::Mat& dst, float fGamma);
+    int method;
+    bool doGamaCorrection;
+    string coeffFilePath;
+    FileStorage coeffFile;
+    bool trainingMode;
+    double coeff[64];
+    PCA pca;
+    float detectorTH=0;
+
+};
+
+
+#endif //TRACK_BGSDETECTOR_H
