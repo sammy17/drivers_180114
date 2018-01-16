2bd59e8765f0972773ac557a1ac3ddaa011ad2aa
sammy17 <chathura.abeyrathne.lk@gmail.com>
2018-01-16 10:06:54 +0530

    debug 180116

--- maxi_new.cpp
+++ maxi_new.cpp
@@ -348,8 +348,8 @@ int main(int argc, char *argv[]) {
             std::copy ( m_axi_feature+512*q, m_axi_feature+512*(q+1), histogram.begin() );
             // frame.histograms.push_back(histogram);
             for (int l=0;l<512;l++){
-                if(m_axi_feature[512*q+l]!=detector.histograms[q][l])
-                    printf("Mismatch : %d, expected : %d, actual : %d\n",l,detector.histograms[q][l],m_axi_feature[512*q+l])
+                if(m_axi_feature[512*q+l]!=detector.histograms[q].at<unsigned short>(l))
+                    printf("Mismatch : %d, expected : %d, actual : %d\n",l,detector.histograms[q].at<unsigned short>(l),m_axi_feature[512*q+l])
             }
             frame.histograms.push_back(detector.histograms[q]);
         }
